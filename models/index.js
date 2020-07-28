f96bab025a274e184f5742d54f14a72c6af13ad1
Caroline Ninganga <carolineninganga@Carolines-MBP.home>
2020-07-27 20:59:43 -0400

    exported books in models forlder

--- models/index.js
+++ models/index.js
@@ -1,17 +1,4 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const bookSchema = new Schema({
-  title: { type: String, required: true },
-  subtitle: { type: String },
-  authors: { type: [String], required: true },
-  link: { type: String, required: true },
-  description: { type: String, required: true },
-  image: { type: String, required: true },
-  googleId: { type: String, required: true, unique: true }
-});
-
-const Book = mongoose.model("Book", bookSchema);
-
-module.exports = Book;
-
+module.exports = {
+    Book: require("./book")
+  };
+  
